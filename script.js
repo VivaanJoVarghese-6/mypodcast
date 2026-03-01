ff9ff5837ca44b93ea64b4836972e304b24b7fb4
VivaanJoVarghese-6 <vivaanjovarghese@gmail.com>
2026-03-01 22:49:00 +0530

    script.js

--- script.js
+++ script.js
@@ -26,7 +26,11 @@ app.post('/register', async (req, res) => {
 });
 
 // 🔹 Start Server
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
     console.log("Server running on port 3000");
 });
 app.use(express.static('public'));
