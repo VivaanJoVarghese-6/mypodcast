e6ccad121b4ff29cb6881ca17d8fee4d6c576669
VivaanJoVarghese-6 <vivaanjovarghese@gmail.com>
2026-03-03 01:30:09 +0530

    script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const { Resend } = require("resend");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static('public'));
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // ===================================================
